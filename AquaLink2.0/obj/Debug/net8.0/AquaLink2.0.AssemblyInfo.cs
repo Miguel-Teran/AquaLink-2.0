79365b675a907e94d2a739abc0ea1f42a2dec675
Mik3g-2 <josetv534@gmail.com>
2026-03-18 09:55:46 -0700

    string de conexion a somee

--- AquaLink2.0/obj/Debug/net8.0/AquaLink2.0.AssemblyInfo.cs
+++ AquaLink2.0/obj/Debug/net8.0/AquaLink2.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AquaLink2.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c6c758c036f1f57733ed352655d2318d5ffc003")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ffb7443af50fbd0b1e758de97448ca46540cb58")]
 [assembly: System.Reflection.AssemblyProductAttribute("AquaLink2.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AquaLink2.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
